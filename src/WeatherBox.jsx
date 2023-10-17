4406d0f7b0e8a55fd64cf6a085e6f5cbf2469cab
VishalPandit0 <vishalkumar14471@gmail.com>
2023-10-17 11:43:55 +0530

    just checking

--- src/WeatherBox.jsx
+++ src/WeatherBox.jsx
@@ -18,7 +18,7 @@ import moonset from '../src/assets/moonset.png'
 const WeatherBox = () => {
   const [city, setCity] = useState('Delhi');
   const [weatherinfo, setWeatherinfo] = useState(null);
- 
+
 
 
   useEffect(() => {
@@ -31,9 +31,9 @@ const WeatherBox = () => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`
     fetch(url)
       .then((response) => response.json())
-      .then((data) =>{
-        
-        const weather ={
+      .then((data) => {
+
+        const weather = {
           location: data.name,
           country: data.sys.country,
           temprature: data.main.temp,
@@ -45,142 +45,142 @@ const WeatherBox = () => {
           mintemp: data.main.temp_min,
           sunrise: data.sys.sunrise,
           sunset: data.sys.sunset,
-          timezone:data.timezone,
-          Tempcond:data.weather[0].description,
-          Tempcond2:data.weather[0].main
+          timezone: data.timezone,
+          Tempcond: data.weather[0].description,
+          Tempcond2: data.weather[0].main
 
         }
         setWeatherinfo(weather);
       }
       )
-      .catch(err=>{console.log(err)})
-    }
+      .catch(err => { console.log(err) })
+  }
 
-    const handleKeyPress = (e) => {
-      if (e.key === "Enter") {
-        getweather();
-      }
-    };
-    
-  
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      getweather();
+    }
+  };
+
+
 
 
   return (
-  <>
-    {weatherinfo &&
-       
-  
-    <div className='app'  
-     
-    style={ weatherinfo.Tempcond2==='clear'? { backgroundImage: `url(${clear})` }:
-           weatherinfo.Tempcond2==='haze'? { backgroundImage: `url(${mist})` }:
-           weatherinfo.Tempcond2==='rainy'? { backgroundImage: `url(${Rainy})` }:
-            { backgroundImage: `url(${Rainy})` }
-   }>
-    <div>
-      <nav className="top">
-        <div className="logo">
-          <div className="logoimg"><img src={sun_logo} alt="mylogo" /></div>
-          <h3> WeatherCheck </h3>
-        </div>
-        <div className="searchBox"><input type='text' placeholder='Enter a city' onKeyPress={handleKeyPress} onChange={(e) => setCity(e.target.value)}/>
-          <div className="searchButton"><button className='get' onClick={getweather}>Get</button></div>
-          </div>
-      </nav>
+    <>
+      {weatherinfo &&
 
-      <div className="container">
-        <div className='insideContainer'>
-        
-          <div className="location">
-            <h2> {weatherinfo.location}, {weatherinfo.country}  </h2>
-          </div>
 
-          <div className="temprature">
-            <div className="tempimg"><img src={sun_logo} alt="weatherLike" /></div>
-            <h1>{weatherinfo.temprature.toFixed()}</h1>
-            <h1>&deg;</h1>
-            <div className="cOrF">
-            <div className="celcius " >
-              C
+        <div className='app'
+
+          style={weatherinfo.Tempcond2 === 'clear' ? { backgroundImage: `url(${clear})` } :
+            weatherinfo.Tempcond2 === 'haze' ? { backgroundImage: `url(${mist})` } :
+              weatherinfo.Tempcond2 === 'rainy' ? { backgroundImage: `url(${Rainy})` } :
+                { backgroundImage: `url(${Rainy})` }
+          }>
+          <div>
+            <nav className="top">
+              <div className="logo">
+                <div className="logoimg"><img src={sun_logo} alt="mylogo" /></div>
+                <h3> WeatherCheck </h3>
+              </div>
+              <div className="searchBox"><input type='text' placeholder='Enter a city' onKeyPress={handleKeyPress} onChange={(e) => setCity(e.target.value)} />
+                <div className="searchButton"><button className='get' onClick={getweather}>Get</button></div>
+              </div>
+            </nav>
+
+            <div className="container">
+              <div className='insideContainer'>
+
+                <div className="location">
+                  <h2> {weatherinfo.location}, {weatherinfo.country}  </h2>
+                </div>
+
+                <div className="temprature">
+                  <div className="tempimg"><img src={sun_logo} alt="weatherLike" /></div>
+                  <h1>{weatherinfo.temprature.toFixed()}</h1>
+                  <h1>&deg;</h1>
+                  <div className="cOrF">
+                    <div className="celcius " >
+                      C
+                    </div>
+                    <div className='farenheit' >
+                      F
+                    </div>
+                  </div>
+                </div>
+
+                <div className="feelslike">
+                  <h1> {weatherinfo.Tempcond} </h1>
+                </div>
+              </div>
             </div>
-            <div className='farenheit' >
-              F
+            <div className="above-extra">
+              <div className="fordiv">
+                <img src={thermometer} alt="" />
+                <p>  Feels like {weatherinfo.feels_like.toFixed()}&deg;</p>
+              </div>
+              <div className="fordiv">
+                <img src={windy} alt="" />
+                ,<p>Wind {weatherinfo.wind}km/h </p>
+              </div>
+              <div className="fordiv">
+                <img src={humidity} alt="" />
+                <p> Humidity {weatherinfo.humidity}% </p>
+              </div>
+              <div className="fordiv">
+                <img src={visibility} alt="" />
+                <p>  Visibility {weatherinfo.visibility}m</p>
+              </div>
+
+            </div>
+
+            <br />
+            <h3 className="tittle-day-details">Day Details</h3>
+            <div className="container2">
+              <div className="day-details">
+                <div className="highAndLow">
+                  <h4>Day</h4>
+                  <p>
+                    There will be mostly sunny skies.The high will be {weatherinfo.maxtemp} &deg;
+
+                  </p>
+                  <h4>Night</h4>
+                  <p>
+                    The skies will be clear. The low will be  {weatherinfo.mintemp} &deg;.
+                  </p>
+                </div>
+              </div>
+
+              <div className="sun">
+                <h4>SUNRISE</h4>
+                <div className="sunrise">
+                  <img src={sunrise} alt="" className="icon" />{" "}
+                  `{moment.utc(weatherinfo.sunrise, 'X').add(weatherinfo.timezone, 'seconds').format('HH:mm a')}`
+                </div>
+                <h4>SUNSET</h4>
+                <div className="sunset">
+                  <img src={sunset} alt="" className="icon" />{" "}
+                  `{moment.utc(weatherinfo.sunset, 'X').add(weatherinfo.timezone, 'seconds').format('HH:mm a')}`
+                </div>
+              </div>
+
+              <div className="moon">
+                <h4>MOONRISE</h4>
+                <div className="moonrise">
+                  <img src={moonrise} alt="" className="icon" />{" "}
+                  7:00 PM
+                </div>
+                <h4>MOONSET</h4>
+                <div className='moonset'>
+                  <img src={moonset} alt="" className="icon" />{" "}
+                  4:00 AM
+                </div>
+              </div>
             </div>
           </div>
-          </div>
-          
-          <div className="feelslike">
-            <h1> {weatherinfo.Tempcond} </h1>
-          </div>
         </div>
-      </div>
-      <div className="above-extra">
-          <div className="fordiv">
-            <img src={thermometer} alt="" />
-           <p>  Feels like {weatherinfo.feels_like.toFixed()}&deg;</p> 
-          </div>
-          <div className="fordiv">
-             <img src={windy} alt="" />
-             ,<p>Wind {weatherinfo.wind}km/h </p> 
-          </div>
-          <div className="fordiv">
-             <img src={humidity} alt="" />
-           <p> Humidity {weatherinfo.humidity}% </p> 
-          </div>
-          <div className="fordiv">
-             <img src={visibility} alt="" />
-           <p>  Visibility {weatherinfo.visibility}m</p> 
-          </div>
-      
-        </div>
-
-      <br />
-      <h3 className="tittle-day-details">Day Details</h3>
-      <div className="container2">
-        <div className="day-details">
-          <div className="highAndLow">
-            <h4>Day</h4>
-            <p>
-              There will be mostly sunny skies.The high will be {weatherinfo.maxtemp} &deg;
-              
-            </p>
-            <h4>Night</h4>
-            <p>
-              The skies will be clear. The low will be  {weatherinfo.mintemp} &deg;.
-            </p>
-          </div>
-        </div>
-
-        <div className="sun">
-          <h4>SUNRISE</h4>
-          <div className="sunrise">
-            <img src={sunrise} alt="" className="icon" />{" "}
-            `{moment.utc(weatherinfo.sunrise,'X').add(weatherinfo.timezone,'seconds').format('HH:mm a')}`
-          </div>
-          <h4>SUNSET</h4>
-          <div className="sunset">
-            <img src={sunset} alt="" className="icon" />{" "}
-            `{moment.utc(weatherinfo.sunset,'X').add(weatherinfo.timezone,'seconds').format('HH:mm a')}`
-          </div>
-        </div>
-
-        <div className="moon">
-          <h4>MOONRISE</h4>
-          <div className="moonrise">
-            <img src={moonrise} alt="" className="icon" />{" "}
-            7:00 PM
-          </div>
-          <h4>MOONSET</h4>
-          <div className='moonset'>
-            <img src={moonset} alt="" className="icon" />{" "}
-            4:00 AM
-          </div>
-        </div>
-      </div>
-      </div>
-      </div>
-}
-  </>
+      }
+    </>
 
 
 
